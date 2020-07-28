6dc8b3eb6b1912f398f5458dfe2fa8cc6dd087f6
Tharon Weighill <tharonweighill@gmail.com>
2020-07-28 22:29:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,10 +5,10 @@ def speak_to_grandma(phrase)
   binding.pr
   if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU PUMPKIN"
-elsif phrase == phrase.upcase
-"no, not since 1938!"
-else 
-  "HUH?! SPEAK UP< SONNY!"
+  elsif phrase == phrase.upcase
+  "NO, NOT SINCE 1938"
+  else 
+  "HUH?! SPEAK UP, SONNY!"
 end  
 
 
