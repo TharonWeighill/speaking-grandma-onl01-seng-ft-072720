8db8d62982addcc798b6c67613f6a012bb8e91cf
Tharon Weighill <tharonweighill@gmail.com>
2020-07-28 22:15:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
+require "pry"
 
-def speak_to_grandma
-    if "I LOVE YOU GRANDMA!".capitalize > ""
-      put "I Love you too"
-    elsif "No, Not since 1983!"
-    else "I love you too Pumpkkin"
+def speak_to_grandma(
+  binding.pry
+  
 end  
+
